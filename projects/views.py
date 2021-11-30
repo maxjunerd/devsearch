aa53c4d9818c6d8be77ee006b3b468b32addd8e0
Tonsai <chayanon.tonsai@adastragrp.com>
2021-12-01 01:20:16 +0700

    add project and projects views and remove pycache

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request):
+    return HttpResponse('My Projects')
+
+def project(request, pk):
+    return HttpResponse('Project ' + pk)
